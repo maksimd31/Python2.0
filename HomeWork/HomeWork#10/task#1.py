af2407043bcc3df45e66af78c9c1b46171b26966
Froge <maksimdis31@gmail.com>
2022-11-14 14:30:00 +0300

    j

--- HomeWork/HomeWork#10/task#1.py
+++ HomeWork/HomeWork#10/task#1.py
@@ -1,34 +1,63 @@
-# Написать функцию-декоратор для кеширования значений функции
-# Написать функцию seq(n)
-# n = 0 ....N
-# (1 + n) ** n возвращает [x1, x2, x3, , , , xn]
-
-# Декораторы функций — вызываемые объекты, которые принимают другую функцию в качестве аргумента.
-# с использованием замыкания
-def my_decorator(*args,**kwargs):
-    i = 1
-    while i<=n:
-        print(i)
-        i += 1
+# декоратор с замыканием
+from datetime import datetime
+import logging
+from functools import wraps
 
 
-@my_decorator
+
+def logger(func):
+    def wrapper(*args, **kwargs):
+        # logging.basicConfig(
+        #     level=logging.INFO, filename='log.log',
+        #     format='[%(l1)s] [%(decorator)s]',
+        #     datefmt='%d.%m.%Y %H:%M:%S', encoding='utf-8')
+        logging.basicConfig(filename='log.log', level=logging.DEBUG)
+        start = datetime.now()
+        result = func(*args, **kwargs)
+        timer = datetime.now() - start
+        logging.debug(f'params: {args} timer: {timer}')
+        # logging.info(f'params: {args} timer: {timer}')
+        return result
+    return wrapper
+
+
+def decorator(funk):
+    @wraps(funk)  # вывод времени работы функции
+    def wrapper(*args, **kwargs):
+        stat = datetime.now()
+        result = funk(*args, **kwargs)
+        print(datetime.now() - stat)  # получаем разницу между началом и концом
+        return result
+
+    return wrapper
+
+
+# def decorator(func):
+#     def inner(*args, **kwargs):
+#         print('Старт')
+#         func(*args, **kwargs)
+#         print('Стоп')
+#
+#     return inner
+
+@logger
+# @decorator
 def seq(n):
-    b = (1+n)**n
-    print(b)
-
-seq(3)
-
-# def seq(n):
-#     i = 1
-#     while i<=n:
-#         print(i)
-#         i += 1
-# seq(3)
-# seq(5)
-# seq(10)
-# seq(9)
-# seq(7)
+    # stat = datetime.now()
+    # result = 0
+    lst = []
+    for i in range(n):
+        result = (1 + i) ** i
+        lst.append(result)
+    # print(datetime.now() - stat)  # получаем разницу между началом и концом
+    return lst
 
 
-    
+# print(seq(5)
+# у меня стоит лимит на 4300
+# ValueError: Exceeds the limit (4300) for integer string conversion
+l1 = seq(1370)  # Запускает максимум  1000
+print(l1)
+# 5seq = decorator(seq)
+#
+# seq(6)
